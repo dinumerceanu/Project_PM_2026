fbf147fc6775ccf908445aecb80a5df9e7c08115
dinumerceanu <dinu.eu@yahoo.com>
2026-05-09 16:45:58 +0300

    pb7 button for start + countdown + lights off after random interval

--- Project_PM/src/main.cpp
+++ Project_PM/src/main.cpp
@@ -1,5 +1,9 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+#include "uptime.h"
+#include "usart.h"
 
 int main(void)
 {
@@ -8,11 +12,49 @@ int main(void)
     DDRB |= (1 << PB1);
     DDRB |= (1 << PB2);
 
+    // PB7 start game
+    DDRB &= ~(1 << PB7);
+    PORTB |= (1 << PB7);
+
+    uptime_init();
+    USART0_init(9600);
+    sei();
+
     while (1) {
+        // wait for button press
+        while (PINB & (1 << PB7));
+        // debounce
+        _delay_ms(50);
+        // wait for button release
+        while (!(PINB & (1 << PB7)));
+        // debounce
+        _delay_ms(50);     
+
+        // turn on LEDs one by one
         PORTB |= (1 << PB0);
-        _delay_ms(1000);
+        uint32_t t = uptime_ms();
+        // wait 1s non-blocking
+        while ((uptime_ms() - t) < 1000);
+        
         PORTB |= (1 << PB1);
-        _delay_ms(1000);
+        t = uptime_ms();
+        while ((uptime_ms() - t) < 1000);
+        
         PORTB |= (1 << PB2);
+        t = uptime_ms();
+        while ((uptime_ms() - t) < 1000);
+
+        // random delay 1-5 sec
+        srand(uptime_ms());
+        uint32_t random_delay = (rand() % 5 + 1) * 1000UL;
+        t = uptime_ms();
+        while ((uptime_ms() - t) < random_delay);
+
+        // turn off LEDs
+        PORTB &= ~((1 << PB0) | (1 << PB1) | (1 << PB2));
+
+        USART0_print("LEDs off after: ");
+        USART0_print_u32(random_delay);
+        USART0_print(" ms\r\n");
     }
 }
---
+++ Project_PM/src/uptime.cpp
@@ -0,0 +1,44 @@
+#include "uptime.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+volatile uint32_t g_uptime_ms = 0;
+
+ISR(TIMER2_COMPA_vect)
+{
+    g_uptime_ms++;
+}
+
+void uptime_init(void)
+{
+    /* Timer2 CTC mode */
+    TCCR2A = (1 << WGM21);
+    TCCR2B = 0;
+
+    /*
+        16MHz / 64 = 250000 Hz
+        1ms = 250 ticks - 1 = 249
+    */
+    OCR2A = 249;
+
+    TIMSK2 = (1 << OCIE2A);
+
+    /* start timer with prescaler 64 */
+    TCCR2B = (1 << CS22);
+}
+
+uint32_t uptime_ms(void)
+{
+    uint32_t t;
+    uint8_t sreg = SREG;
+
+    cli();
+    t = g_uptime_ms;
+    SREG = sreg;
+
+    return t;
+}
